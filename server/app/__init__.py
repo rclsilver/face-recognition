a535a710c7d4c019536afe52bbb8d7bac720ea53
Thomas Bétrancourt <thomas@betrancourt.net>
2021-09-17 20:30:45 +0200

    wip logging

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import re
+
 from typing import List
 
 
@@ -17,7 +20,7 @@ def get_app(
     from app.auth import configure_auth
     from app.auth.oidc import OidcAuth
     from app.routers import cameras, identities, recognition, users
-    from fastapi import FastAPI
+    from fastapi import FastAPI, Request, status
 
     # Configure authentication
     configure_auth(OidcAuth, oidc_issuer, source_whitelist)
@@ -31,6 +34,30 @@ def get_app(
         root_path=app_prefix,
         source_whitelist=source_whitelist
     )
+
+    if production:
+        access_logger = logging.getLogger(__name__)
+        re_status = re.compile('^HTTP_[0-9]+_(.+)$')
+        status_strings = { 
+            v: re_status.match(k).group(1).replace('_', ' ') for k, v in status.__dict__.items() if k.startswith('HTTP_')
+        }
+
+        @app.middleware('http')
+        async def access_log(request: Request, call_next):
+            response = await call_next(request)
+
+            access_logger.info(
+                '%s:%d - "%s %s %s" %d %s',
+                request.client.host,
+                request.client.port,
+                request.method,
+                request.url,
+                'HTTP/{}'.format(request.scope['http_version']),
+                response.status_code,
+                status_strings.get(response.status_code, '')
+            )
+
+            return response
 
     # Create the routes
     app.include_router(identities.router, prefix='/identities')
