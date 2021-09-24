73b04d51e0d3e90f9e6f5ed5e2fcfd95bb0456b0
Thomas Bétrancourt <thomas@betrancourt.net>
2021-09-24 17:54:22 +0200

    wip local authent

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -56,6 +56,7 @@ def get_app(
                 response.status_code,
                 status_strings.get(response.status_code, '')
             )
+            access_logger.debug('Headers: %s', request.headers)
 
             return response
 
